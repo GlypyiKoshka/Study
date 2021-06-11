6ad18127d3fdd1077409d8e971bdcc6d81277def
Melnikov <ypypy.zzz@gmail.com>
2021-06-11 18:46:13 +0500

    Added all files

---
+++ src/com/company/main4.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class main4 {
+    public static void main(String args[]) {
+/*
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 1;
+        int n1 = 1;
+        int n2;
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+        System.out.println(sum);
+    }
+}
+*/
+    }}
