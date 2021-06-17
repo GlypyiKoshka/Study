24d1912905c0a764229ee14ac7e65d74bcf08dc0
Melnikov <ypypy.zzz@gmail.com>
2021-06-17 15:37:06 +0500

    fib

--- src/com/company/main4.java
+++ src/com/company/main4.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class main4 {
     public static void main(String args[]) {
-/*
+
         Scanner scanner = new Scanner(System.in);
 
-        int n = 1;
-        int n1 = 1;
-        int n2;
-        for (int i = 1; i <= n; i++) {
-            sum = sum * i;
+        int n;
+
+        n = scanner.nextInt();
+
+        int f1 = 1;
+        int f2 = 1;
+        int f3;
+        for (int i = 0; i < n; i++) {
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3;
         }
-        System.out.println(sum);
+        System.out.println(f1);
     }
 }
-*/
-    }}
