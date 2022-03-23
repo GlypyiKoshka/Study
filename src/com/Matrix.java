89e52292ca9a4d57092d79f1e61791880b6de268
GlypyiKoshka <ypypy.zzz@gmail.com>
2022-03-23 23:08:15 +0300

    xo

--- src/com/Matrix.java
+++ src/com/Matrix.java
@@ -62,5 +62,4 @@ public class Matrix {
         return mas;
     }
 
-
 }
--- src/com/XO.java
+++ src/com/XO.java
@@ -1,15 +1,31 @@
 package com;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class XO {
-    static String[][] field = new String[][] {
-            {"+", "-", "+", "-", "+", "-", "+"}
-    };
-    public static void main(String[] args) {
+/*
+    Два игрока играют в крестики и нолики, Первым ходит крестик.
+    Программа отображает поле после каждого хода, после чего запрашивает ход следующего игрока,
+    Игра продолжается до тех пор, пока кто-то не выиграл, либо пока не наступила ничья.
+    Ничьей будем считать ситуацию, когда все поле заполнено, но никто не выиграл
+*/
 
+public class XO {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[][] field = new String[3][3];
+
+        System.out.println("+-+-+-+");
+        System.out.println("|"+field[0][0]+"|"+field[0][1]+"|"+field[0][2]+"|");
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length ; j++) {
+                field[i][j]=" ";
+                System.out.println(field[i][j]);
+            }
+            System.out.println();
+        }
 
     }
-
 }
